620238c00bc5393ad3995777db7855c47c59acd5
chetanjohal <gjohal@mail.sfsu.edu>
2016-11-18 12:41:35 -0800

    Testing 2

--- 01_FirstApp/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ 01_FirstApp/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+//testing 2
 
 public class QuizActivity extends AppCompatActivity {
 
